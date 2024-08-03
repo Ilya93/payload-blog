97d6f18902ad075ee95ea91ddae79decbee1f235
Ilya Chepiga <chepiga.ilya@gmail.com>
2024-08-03 12:00:12 +0300

    image improve

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,12 +1,23 @@
 'use client'
 import NextImage from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import type { Article } from '../../../payload-types'
+import cssVariables from '../../cssVariables'
+
+const { breakpoints } = cssVariables
 
 export const Card = (props: { article: Article }) => {
-  const { slug, meta, title, description, image } = props.article
-  const sanitizedDescription = description?.replace(/\s/g, ' ')
+  const { slug, title, description, image } = props.article
+  const [showImage, setShowImage] = useState(false)
+
+  useEffect(() => {
+    setShowImage(false)
+  }, [title])
+
+  const sizes = Object.entries(breakpoints)
+    .map(([, value]) => `(max-width: ${value}px) ${value}px`)
+    .join(', ')
 
   return (
     <Link className="not-prose" href={`/${slug}`}>
@@ -17,9 +28,14 @@ export const Card = (props: { article: Article }) => {
             <NextImage
               alt={image.alt || ''}
               fill
+              onLoad={() => {
+                setShowImage(true)
+              }}
+              sizes={sizes}
+              priority
               quality={90}
               src={image.url}
-              style={{ objectFit: 'cover' }}
+              style={{ objectFit: 'cover', display: showImage ? 'block' : 'none' }}
             />
           )}
         </div>
@@ -29,9 +45,7 @@ export const Card = (props: { article: Article }) => {
               <h3 className="text-xl font-bold">{title}</h3>
             </div>
           )}
-          {description && (
-            <div className="mt-2">{description && <p>{sanitizedDescription}</p>}</div>
-          )}
+          {description && <div className="mt-2">{description && <p>{description}</p>}</div>}
         </div>
       </article>
     </Link>
